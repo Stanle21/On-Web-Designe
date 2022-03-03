e027b477b6ab24b6691c3c66bd0bd246e1a846cd
Stanle21 <92270088+Stanle21@users.noreply.github.com>
2022-03-03 18:56:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,169 @@
+let onShoes = [
+    {
+        shoeType: "road",
+        name: "CLOUDSTRATUS",
+        headerImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/39.99208_ON-RUNNING-Tenisky-Damske-CLOUDSTRATUS-Rose-Red.jpg",
+        textDesc: "Zdvojnásobte CloudTec®, zdvojnásobte svoj beh. Výkonná topánka pre maximálne odpruženie na cestách - od 5 km do maratónu.",
+        techImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/cloudultra-rose-side.jpg",
+        techHead: "TECHNOLÓGIA",
+        techSecondHead: "VIAC OBLÁČIKOV, VIAC ENERGIE",
+        techDescription: "Cloudstratus obsahuje dve vrstvy CloudTec® obohatené o Helion™ prezvýšené vertikálne a horizontálne odpruženie. Prepojené prvky Cloud v prednej časti chodidla sú dokonale zladené so Speedboard®, čo má za následok veľkú návratnosť energie, odolnosť a ochranu na ceste.",
+        materialHead: "MATERIÁL",
+        matSecondHead: "ZAČNITE BEH RÝCHLEJŠIE",
+        matDescription: "Približne 75 % polyesteru použitého v odolnom a dynamickom zvršku je recyklovaných.Vďaka široko priliehajúcej prednej časti chodidla a vylepšenému držaniu päty, zakončenému systémom hviezdicového šnurovania, je Cloudstratus vysoko inkluzívny a ideálny pre všetky typy bežcov.Gumená priľnavosť na podrážke poskytuje trakciu na ceste.",
+        designeHead: "DIZAJN",
+        designeSecondHead: "ZDVOJNÁSOBTE SVOJ BEH",
+        designeDescription: "Maximálne odpruženie, maximálny výkon. Odvážte sa ísť ďalej v topánke pre bežcov, ktorí vyžadujú viac.Double Clouds sa stretávajú s prepracovanou medzipodrážkou, recyklovaným zvrškom a prepracovanou vnútornou konštrukciou.Je to pohodlie a akurátne množstvo podpory."
+    },
+    {
+        shoeType: "road",
+        name: "CLOUDSTRATUS",
+        headerImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/39.99212_ON-RUNNING-Tenisky-Panske-CLOUDSTRATUS-White-Black.jpg",
+        textDesc: "Zdvojnásobte CloudTec®, zdvojnásobte svoj beh. Výkonná topánka pre maximálne odpruženie na cestách - od 5 km do maratónu.",
+        techImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/cloudultra-white-side.jpg",
+        techHead: "TECHNOLÓGIA",
+        techSecondHead: "VIAC OBLÁČIKOV, VIAC ENERGIE",
+        techDescription: "Cloudstratus obsahuje dve vrstvy CloudTec® obohatené o Helion™ prezvýšené vertikálne a horizontálne odpruženie. Prepojené prvky Cloud v prednej časti chodidla sú dokonale zladené so Speedboard®, čo má za následok veľkú návratnosť energie, odolnosť a ochranu na ceste.",
+        materialHead: "MATERIÁL",
+        matSecondHead: "ZAČNITE BEH RÝCHLEJŠIE",
+        matDescription: "Približne 75 % polyesteru použitého v odolnom a dynamickom zvršku je recyklovaných.Vďaka široko priliehajúcej prednej časti chodidla a vylepšenému držaniu päty, zakončenému systémom hviezdicového šnurovania, je Cloudstratus vysoko inkluzívny a ideálny pre všetky typy bežcov.Gumená priľnavosť na podrážke poskytuje trakciu na ceste.",
+        designeHead: "DIZAJN",
+        designeSecondHead: "ZDVOJNÁSOBTE SVOJ BEH",
+        designeDescription: "Maximálne odpruženie, maximálny výkon. Odvážte sa ísť ďalej v topánke pre bežcov, ktorí vyžadujú viac.Double Clouds sa stretávajú s prepracovanou medzipodrážkou, recyklovaným zvrškom a prepracovanou vnútornou konštrukciou.Je to pohodlie a akurátne množstvo podpory."
+    },
+    {
+        shoeType: "road",
+        name: "CLOUDSWIFT",
+        headerImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/41.99227_ON-RUNNING-Bezecke-Tenisky-Panske-CLOUDSWIFT-Magnet-Citron.jpg",
+        textDesc: "Ľahký a rýchly s ochranou proti nárazu. Ponúka superpenu Helion ™, takže beh na tvrdých povrchoch nebol nikdy jednoduchší.",
+        techImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/cloudswift-citrone.jpg",
+        techHead: "TECHNOLÓGIA",
+        techSecondHead: "ULTIMÁTNY MESTSKÝ BEH",
+        techDescription: "Zmenená poloha CloudTec® v superpene Helion™ ponúka zvýšené odpruženie prednej časti chodidla a zväčšené obláčiky vzadu tlmia nárazy päty.Extrémna rocker podošva a prepracovaný Speedboard™ optimalizujú prirodzený valivý pohyb pre ešte väčšiu hnaciu silu, ktorá pôsobí hladko a bez námahy.",
+        materialHead: "MATERIÁL",
+        matSecondHead: "RECYKLOVANÉ PREPRACOVANIE",
+        matDescription: "Recyklovaný zvršok zo sieťoviny ponúka ľahké nastupovanie, úplné pohodlie a ideálne sadne. Spolu s podšívkou, šnúrkami a vnútornými výstužami v prednej časti chodidla zaisťuje lepšie držanie v celej topánke. Nové gumené gripy ponúkajú lepšiu trakciu aj na mokrom povrchu.",
+        designeHead: "DIZAJN",
+        designeSecondHead: "ZDOLAJTE BETÓN",
+        designeDescription: "Cloudswift je stvorený na beh v meste. Je ľahký a agilný, ale absorbuje ťažké nárazy, aby vás ochránil pred tvrdým povrchom.Zvršok je navrhnutý pre úplné pohodlie a bezpečnosť, švajčiarsky navrhnutý tak, aby sedel ako vaše obľúbené ponožky.Beh na tvrdých povrchoch nebol nikdy jednoduchší."
+    },
+    {
+        shoeType: "allday",
+        name: "CLOUDSWIFT",
+        headerImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/41.99579_ON-RUNNING-Bezecke-Tenisky-Damske-CLOUDSWIFT-GlacierWhite.jpg",
+        textDesc: "Ľahký a rýchly s ochranou proti nárazu. Ponúka superpenu Helion ™, takže beh na tvrdých povrchoch nebol nikdy jednoduchší.",
+        techImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/cloudswift-white-side.jpg",
+        techHead: "TECHNOLÓGIA",
+        techSecondHead: "ULTIMÁTNY MESTSKÝ BEH",
+        techDescription: "Zmenená poloha CloudTec® v superpene Helion™ ponúka zvýšené odpruženie prednej časti chodidla a zväčšené obláčiky vzadu tlmia nárazy päty.Extrémna rocker podošva a prepracovaný Speedboard™ optimalizujú prirodzený valivý pohyb pre ešte väčšiu hnaciu silu, ktorá pôsobí hladko a bez námahy.",
+        materialHead: "MATERIÁL",
+        matSecondHead: "RECYKLOVANÉ PREPRACOVANIE",
+        matDescription: "Recyklovaný zvršok zo sieťoviny ponúka ľahké nastupovanie, úplné pohodlie a ideálne sadne. Spolu s podšívkou, šnúrkami a vnútornými výstužami v prednej časti chodidla zaisťuje lepšie držanie v celej topánke. Nové gumené gripy ponúkajú lepšiu trakciu aj na mokrom povrchu.",
+        designeHead: "DIZAJN",
+        designeSecondHead: "ZDOLAJTE BETÓN",
+        designeDescription: "Cloudswift je stvorený na beh v meste. Je ľahký a agilný, ale absorbuje ťažké nárazy, aby vás ochránil pred tvrdým povrchom.Zvršok je navrhnutý pre úplné pohodlie a bezpečnosť, švajčiarsky navrhnutý tak, aby sedel ako vaše obľúbené ponožky.Beh na tvrdých povrchoch nebol nikdy jednoduchší."
+    },
+    {
+        shoeType: "allday",
+        name: "CLOUDFLOW",
+        headerImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/21.99588_ON-RUNNING-Bezecke-Tenisky-Panske-CLOUDFLOW-FlareDawn.jpg",
+        textDesc: "Ľahké, rýchle a plne odpružené. Absolvujte tréning a preteky v úplnom pohodlí.",
+        techImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/cloudflow-flaredawn.jpg",
+        techHead: "TECHNOLÓGIA",
+        techSecondHead: "ŽIADEN KOMPROMIS V KOMFORTE",
+        techDescription: "Cloudflow sprístupňuje technológiu elitnej úrovne každému bežcovi. CloudTec® v ultraľahkej superpene Helion™ sa spája pre rýchly a citlivý pocit s plne odpruženou ochranou na ceste.Skonštruovaný zvršok zo sieťoviny ponúka upokojujúce bezpečné uchytenie, či už trénuješ alebo beháš.",
+        materialHead: "MATERIÁL",
+        matSecondHead: "VYROBENÉ PRE VIAC, S MENEJ",
+        matDescription: "Prepracovaný zvršok vytvára efektívnejší vzhľad pre bezproblémové pohodlie. Nie je to len štýlové, ale je to dobré aj pre planétu – s použitím 70 % recyklovaného polyesteru. Starostlivo umiestnené gumené trakčné podložky znamenajú, že pretáčanie úzkych zákrut je pri vysokých rýchlostiach jednoduchšie.",
+        designeHead: "DIZAJN",
+        designeSecondHead: "SKRATKA K BEŽECKÉMU MAXIMU",
+        designeDescription: "Cloudflow vyvinutý s elitnými športovcami, dokáže všetko a je rýchlejší ako kedykoľvek predtým. Výbušný Speedboard® zaťaží každý krok energiou podobnou pružine. Je navrhnutý pre vysoké rýchlosti a maximálny komfort, či už bežíte 5K alebo maratón."
+    },
+    {
+        shoeType: "trail",
+        name: "CLOUD X SHIFT",
+        headerImg: "https://www.velosprint.sk/user/documents/on%20running/FW21/38.99127_ON-RUNNING-Bezecke-Tenisky-Pamske-CLOUD-X-Shift-Vapor-Acacia.jpg",
+        textDesc: "ON Running Cloud X Shift Vapor/Acacia v tejto univerzálnej výkonnej obuvi s pridanou osobnosťou sa stretáva pohodlie a energia.",
+        techImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/CloudXShift-alloy.jpg",
+        techHead: "TECHNOLÓGIA",
+        techSecondHead: "MÄKKÉ PRISTÁTIA, ZAKAŽDÝM",
+        techDescription: "Naša podošva CloudTec®, vyrobená zo superpeny Helion™, absorbuje nárazy v každom smere. Tieto prvky cloudu spolupracujú a reagujú na výbušné pohyby, aby ste dosiahli agilný a adaptívny krok – nech ste kdekoľvek.",
+        materialHead: "MATERIÁL",
+        matSecondHead: "VYJADRITE SA",
+        matDescription: "Nový Cloud X Shift je pre tých, ktorí zvažujú svoju aktívnu výbavu do posledného detailu. Priesvitný zvršok z monomesh je vystužený pre väčšie pohodlie a odolnosť. Vrstva so špeciálne navrhnutými ponožkami On pre svieži, jedinečný štýl, ktorý je viditeľný na každom kroku.",
+        designeHead: "DIZAJN",
+        designeSecondHead: "REMIX SPĹŇA RECYKLÁCIU",
+        designeDescription: "Zatiaľ čo vzhľad je návratom do 90. rokov, zvršok je pevne zameraný dopredu. Tento remix predstavuje recyklované materiály našej milovanej siluete Cloud X. V skutočnosti je viac ako 50 % polyesteru použitého v tejto obuvi recyklovaných. Vyniknite zo všetkých správnych dôvodov."
+    },
+    {
+        shoeType: "trail",
+        name: "CLOUD X SHIFT",
+        headerImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/40.99595_ON-RUNNING-Bezecke-Tenisky-Pamske-CLOUD-X-GlacierOlive.jpg",
+        textDesc: "ON Running Cloud X Shift Vapor/Acacia v tejto univerzálnej výkonnej obuvi s pridanou osobnosťou sa stretáva pohodlie a energia.",
+        techImg: "https://www.velosprint.sk/user/documents/on%20running/SS21/CloudX-olive.jpg",
+        techHead: "TECHNOLÓGIA",
+        techSecondHead: "MÄKKÉ PRISTÁTIA, ZAKAŽDÝM",
+        techDescription: "Naša podošva CloudTec®, vyrobená zo superpeny Helion™, absorbuje nárazy v každom smere. Tieto prvky cloudu spolupracujú a reagujú na výbušné pohyby, aby ste dosiahli agilný a adaptívny krok – nech ste kdekoľvek.",
+        materialHead: "MATERIÁL",
+        matSecondHead: "VYJADRITE SA",
+        matDescription: "Nový Cloud X Shift je pre tých, ktorí zvažujú svoju aktívnu výbavu do posledného detailu. Priesvitný zvršok z monomesh je vystužený pre väčšie pohodlie a odolnosť. Vrstva so špeciálne navrhnutými ponožkami On pre svieži, jedinečný štýl, ktorý je viditeľný na každom kroku.",
+        designeHead: "DIZAJN",
+        designeSecondHead: "REMIX SPĹŇA RECYKLÁCIU",
+        designeDescription: "Zatiaľ čo vzhľad je návratom do 90. rokov, zvršok je pevne zameraný dopredu. Tento remix predstavuje recyklované materiály našej milovanej siluete Cloud X. V skutočnosti je viac ako 50 % polyesteru použitého v tejto obuvi recyklovaných. Vyniknite zo všetkých správnych dôvodov."
+    },
+];
+
+function DisplaySelectedShoes(selected) {
+
+    let filterRoadShoes = onShoes.filter(item => item.shoeType === selected);
+    let inputShoesContent = document.getElementById("shoes-content");
+    
+    inputShoesContent.innerHTML = "";
+
+    filterRoadShoes.forEach(item => {
+        inputShoesContent.innerHTML += `
+        <div class="on-shoes-content">
+            <div class="on-shoes-image-wrapper">
+                <img class="onshoes-img"
+                    src='${item.headerImg}'
+                    alt=" CloudStratus">
+                <h3 class="on-shoes-head-text">${item.name}</h3>
+            </div>
+            <div class="on-shoes-text">
+                <p class="on-shoes-paragraf-text">${item.textDesc}</p>
+            </div>
+            <div class="on-shoes-technologies">
+                <img class="head-technologies-image"
+                    src='${item.techImg}' alt="CloudStratus">
+                <div class="on-shoes-technologies-wrapper">
+                    <div class="on-shoes-technologies-content">
+                        <h3 class="technologies-header-text">${item.techHead}</h3>
+                        <div class="on-technologie-content">
+                            <h4 class="header-text-content">${item.techHead}</h4>
+                            <h2 class="header-text-content">${item.techSecondHead}</h2>
+                            <p class="header-text-content">${item.techDescription}</p>
+                        </div>
+                    </div>
+                    <div class="on-shoes-technologies-content">
+                        <h3 class="technologies-header-text">${item.materialHead}</h3>
+                        <div class="on-technologie-content">
+                            <h4 class="header-text-content">${item.materialHead}</h4>
+                            <h2 class="header-text-content">${item.matSecondHead}</h2>
+                            <p class="header-text-content">${item.matDescription}</p>
+                        </div>
+                    </div>
+                    <div class="on-shoes-technologies-content">
+                        <h3 class="technologies-header-text">${item.designeHead}</h3>
+                        <div class="on-technologie-content">
+                            <h4 class="header-text-content">${item.designeHead}</h4>
+                            <h2 class="header-text-content">${item.designeSecondHead}</h2>
+                            <p class="header-text-content">${item.designeDescription}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        `
+    });
+};
+
